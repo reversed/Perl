7184fc50edf01604ac412cc4f18db39cc426a304
reversed <leichen.usst@gmail.com>
2016-08-16 04:30:53 +0800

    upload ex

---
+++ LearningPerl/ex2-2.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+print "Radius: ";
+chomp (my $radius = <STDIN>);
+
+my $result = 2*3.141592654*$radius;
+
+print "$result\n";
+
+
+
+
+
+
+
+
+
+
+
+
